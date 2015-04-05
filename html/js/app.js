6f4b34d9a66983947b829ecb1b09b48523148664
Steven Carroll <stevencarroll90@gmail.com>
2015-04-05 15:41:45 -0600

    Added a 404 partial page.  Renamed user.html partial to profile.html.  The (username) navbar button now takes you to the profile page.

--- html/js/app.js
+++ html/js/app.js
@@ -3,12 +3,13 @@ angular.module('app', ['ui.router', 'byu.controllers'])
     function($stateProvider, $urlRouterProvider){
 
     $urlRouterProvider.when('', 'home'); //base page is home page
+    $urlRouterProvider.when('/', 'home'); //base page is home page
     $urlRouterProvider.otherwise("/404/"); //if none of the states match, go to 404 error
 
     $stateProvider.state('404', {
-        url:'/404/',
-        title:"404 - page not found",
-        templateUrl:""
+        url:'/404',
+        templateUrl:"partials/404.html",
+        title:"404 - page not found"
     });
 
     $stateProvider.state('home', {
@@ -21,14 +22,14 @@ angular.module('app', ['ui.router', 'byu.controllers'])
     $stateProvider.state('login', {
         url:'/login',
         templateUrl: 'partials/login.html',
-        controller:"",
+        controller: "",
         title: 'Login'
     });
 
     $stateProvider.state('register', {
         url:'/register',
         templateUrl: 'partials/register.html',
-        controller:"",
+        controller: "user",
         title: 'Welcome! Please Register'
     });
 
@@ -37,8 +38,8 @@ angular.module('app', ['ui.router', 'byu.controllers'])
         resolve:{
             userID:function($stateParams){return $stateParams.userID},
         },
-        templateUrl: '',
-        controller:"",
+        templateUrl: "partials/profile.html",
+        controller: "user",
         title: 'User Profile'
     });
 
--- html/js/nav_bar.js
+++ html/js/nav_bar.js
@@ -1,7 +1,6 @@
 angular.module("byu.controllers.navBar", [])
     .controller("navBar", ['$scope', '$state', function($scope, $state){
         $scope.isActive = function(viewLocation) {
-            console.log($state);
             return viewLocation === $state.current.name;
         }
     }]);
