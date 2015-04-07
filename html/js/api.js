f5a9161aeccad4fdbfb930f188420e2c7489e1cc
Scott Hampton <scott.l.hampton@gmail.com>
2015-04-07 02:57:54 +0000

    Put callbacks on all of the api.js server calls.

--- html/js/api.js
+++ html/js/api.js
@@ -15,35 +15,66 @@ angular.module("byu.api", [])
             });
         },
         insertUser: function(){
-            return $http.post("/insertuser")
+            return $http.post("/insertuser"
+            ).success(function(result){
+                return result;
+            }).error(function(result){
+                return result;
+            });
         },
         updateUser: function(){
-            return $http.post("/updateuser")
+            return $http.post("/updateuser"
+            ).success(function(result){
+                return result;
+            }).error(function(result){
+                return result;
+            });
         },
         removeUser: function(){
-            return $http.post("/removeuser")
+            return $http.post("/removeuser"
+            ).success(function(result){
+                return result;
+            }).error(function(result){
+                return result;
+            });
         },
         find: function(title){
             return $http.post("/find",
             {
               "Title": title
-            })
+            }).success(function(result){
+                return result;
+            }).error(function(result){
+                return result;
+            });
         },
         getAllItems: function(){
-            return $http.get("/get_all_items")
+            return $http.get("/get_all_items"
+            ).success(function(result){
+                return result;
+            }).error(function(result){
+                return result;
+            });
         },
         getItem: function(id){
-            console.log(id);
             return $http.post('/get_item',
             {
               "ID": id
+            }).success(function(result){
+                return result;
+            }).error(function(result){
+                return result;
             });
         },
         getCategoryItems: function(category){
             return $http.post("/get_category_item",
             {
               "Category": category
-            })
+            }).success(function(result){
+                return result;
+            }).error(function(result){
+                return result;
+            });
         },
         postItem: function(title, path, user, date, category){
             return $http.post("/post_item",
@@ -55,13 +86,21 @@ angular.module("byu.api", [])
               "Category": category,
               "Bought": false,
               "Comments": []
-            })
+            }).success(function(result){
+                return result;
+            }).error(function(result){
+                return result;
+            });
         },
         deleteItem: function(id){
             return $http.post("/delete_item",
             {
               "ID": id
-            })
+            }).success(function(result){
+                return result;
+            }).error(function(result){
+                return result;
+            });
         },
         comment: function(itemID, user, comment){
             return $http.post("/comment",
@@ -69,10 +108,19 @@ angular.module("byu.api", [])
               "ID": itemID,
               "UserID": user,
               "Comment": comment
-            })
+            }).success(function(result){
+                return result;
+            }).error(function(result){
+                return result;
+            });
         },
         removeAllItems: function(){
-            return $http.post("/remove_all_items")
+            return $http.post("/remove_all_items"
+            ).success(function(result){
+                return result;
+            }).error(function(result){
+                return result;
+            });
         }
     }
 }])
