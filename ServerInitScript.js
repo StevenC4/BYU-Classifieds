1b6a281bd37cd5f765446b22ffcd1ea6c4d3fd9f
osoguasu <osoguasu@gmail.com>
2015-04-08 17:54:48 -0600

    Working on a script to init a local server
    This way, we can have similar test data. to use, navigate to folder in
    mongo shell, and type `load("ServerInitScript.js")`. This is still a
    work in progress, but should prove useful.

---
+++ ServerInitScript.js
@@ -0,0 +1,51 @@
+﻿var conn = new Mongo();
+var db = conn.getDB("byu-classifieds");
+
+var usersToInsert = [
+    { "username": "sean" },
+    { "username": "michael" },
+    { "username": "steve" },
+    { "username": "scott" }];
+
+db.users.insert(usersToInsert);
+
+var adsToInsert = [
+    {
+        "title": "something awesome1",
+        "picture": "",
+        "userID": db.users.findOne({ 'username': 'sean' })._id,
+        "postDate": "",
+        "category": "",
+        "bought": 0,
+        "comments": []
+    },
+    {
+        "title": "something awesome2",
+        "picture": "",
+        "userID": db.users.findOne({ 'username': 'sean' })._id,
+        "postDate": "",
+        "category": "",
+        "bought": 0,
+        "comments": []
+    },
+    {
+        "title": "something awesome3",
+        "picture": "",
+        "userID": db.users.findOne({ 'username': 'sean' })._id,
+        "postDate": "",
+        "category": "",
+        "bought": 0,
+        "comments": []
+    },
+    {
+        "title": "something awesome4",
+        "picture": "",
+        "userID": db.users.findOne({ 'username': 'scott' })._id,
+        "postDate": "",
+        "category": "",
+        "bought": 0,
+        "comments": []
+    }
+];
+db.ads.insert(adsToInsert);
+
