0a76b0561daf58f1f75561ad07c2a4e7318dc874
Steven Carroll <stevencarroll90@gmail.com>
2015-04-13 11:34:56 -0600

    Cleaned up browser caching of user info

--- html/js/nav_bar.js
+++ html/js/nav_bar.js
@@ -2,15 +2,12 @@ angular.module("byu.controllers.navBar", ['byu.controllers.user', 'byu.usercooki
     .controller("navBar", ['$scope', '$state', 'userCookie', function($scope, $state, userCookie){
         $scope.user = userCookie.getUser();
         $scope.hasUser = function() {
-            console.log('Tested');
             return userCookie.getUser() !== null;
-        }
+        };
         $scope.isActive = function(viewLocation) {
             return viewLocation === $state.current.name;
         };
         $scope.logout = function() {
-            //console.log("logout");
             userCookie.unsetUser();
-            //$scope.user = userCookie.getUser();
         }
     }]);
